67212b7c46653f318facc10668a6bd4ce9d6d018
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-11-02 17:46:50 -0300

    adjusts on middleware JWT token

--- Back_End/config/middleware.js
+++ Back_End/config/middleware.js
@@ -4,9 +4,10 @@ module.exports = function (application) {
 // Adding Middleware wich will request the token on following function
 application.use(function (req, res, next) {
     let path = req.path
+    let id = path.length-1
     let userData = {}
         userData["token"] = req.body.token || req.headers["token"] || req.query.token;
-        userData["id"] = path.split('/')[4] ;
+        userData["id"] = path[id] ;
     
     let appData = {};
     
@@ -17,10 +18,10 @@ application.use(function (req, res, next) {
                 appData["data"] = "Token is invalid !";
                 res.status(500).json(appData);
             } else if (payload.id != userData.id){
-                next();
-                //appData["error"] = 1;
-                //appData["data"] = "Invalid User! Please sign in";
-                //res.status(403).json(appData);
+                // next();
+                appData["error"] = 1;
+                appData["data"] = "Invalid User! Please sign in";
+                res.status(403).json(appData);
             } else {
                 next();
             }
@@ -28,8 +29,8 @@ application.use(function (req, res, next) {
     } else {
         appData["error"] = 1;
         appData["data"] = "Please send a token or login";
-        res.redirect(301, "http://10.1.0.101:80/")
-        // res.status(403).json(appData);
+        // res.redirect(301, "http://10.1.0.101:80/")
+        res.status(403).json(appData);
     }
 });
 }

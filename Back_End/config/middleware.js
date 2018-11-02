f3aba93112fd09e378ec233f6f81548d17fc257e
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-11-02 17:51:55 -0300

    Adjusts on JWT verification

--- Back_End/config/middleware.js
+++ Back_End/config/middleware.js
@@ -18,7 +18,6 @@ application.use(function (req, res, next) {
                 appData["data"] = "Token is invalid !";
                 res.status(500).json(appData);
             } else if (payload.id != userData.id){
-                // next();
                 appData["error"] = 1;
                 appData["data"] = "Invalid User! Please sign in";
                 res.status(403).json(appData);
@@ -29,8 +28,8 @@ application.use(function (req, res, next) {
     } else {
         appData["error"] = 1;
         appData["data"] = "Please send a token or login";
-        // res.redirect(301, "http://10.1.0.101:80/")
-        res.status(403).json(appData);
+        res.redirect(301, "http://10.1.0.101:80/")
+        // res.status(403).json(appData);
     }
 });
 }

181160c8286d8eeed05bd248b3a3d65a9bef970e
wrj95 <juniortrimundial@hotmail.com>
2018-11-04 19:14:41 -0200

    Solicitacao historic implemented

--- Back_End/api/models/userDAO.js
+++ Back_End/api/models/userDAO.js
@@ -22,6 +22,27 @@ userDAO.prototype.listUsers = function (callback) {
     this._connection.query("SELECT * FROM mydatabase.tab_usuario", callback)
 }
 
+userDAO.prototype.listSolicitacao = function (userData, callback) {
+    this._connection.query("SELECT cod_solicitacao, tit_solicitacao, status FROM tab_solicitacao WHERE cod_usuario = ?", userData, callback)
+}
+
+userDAO.prototype.detailsSolicitacao = function (userData, callback) {
+    this._connection.query("SELECT s.tit_solicitacao AS tit_solicitacao, ufo.des_uf AS uf_origem, muno.des_municipio AS mun_origem, " 
+                            + "CONCAT(eo.endereco ,  ', ' , eo.numero, ' - ', eo.complemento) AS end_origem_completo, ufd.des_uf AS uf_destino, "
+                            + "mund.des_municipio AS mun_destino, CONCAT(ed.endereco,', ', ed.numero, ' - ', ed.complemento) AS end_destino_completo, "
+                            + "DATE_FORMAT(s.data_servico, '%d/%m/%Y') AS data, s.hora_servico AS hora, s.des_solicitacao AS descricao, "
+                            + "s.vlr_estimado_carga AS valor, s.status " 
+                            + "FROM tab_solicitacao s " 
+                            + "INNER JOIN tab_usuario u ON u.cod_usuario = s.cod_usuario "
+                            + "LEFT JOIN tab_endereco eo ON eo.cod_usu_emp = s.cod_usuario AND eo.flg_usu_emp = 'U' AND eo.cod_endereco = s.cod_endereco_origem "
+                            + "INNER JOIN tab_uf ufo ON ufo.cod_uf = eo.cod_uf "
+                            + "INNER JOIN tab_municipio muno ON muno.cod_municipio = eo.cod_municipio "
+                            + "LEFT JOIN tab_endereco ed ON ed.cod_usu_emp = s.cod_usuario AND ed.flg_usu_emp = 'U' AND ed.cod_endereco = s.cod_endereco_destino "
+                            + "INNER JOIN tab_uf ufd ON ufd.cod_uf = ed.cod_uf "
+                            + "INNER JOIN tab_municipio mund ON mund.cod_municipio = ed.cod_municipio "
+                            + "WHERE s.cod_solicitacao = ?", userData, callback)
+}
+
 //Select catch the address registered by user  
 userDAO.prototype.getAddress = function (userData, callback){
     this._connection.query("SELECT cod_endereco, endereco FROM mydatabase.tab_endereco WHERE cod_usu_emp = ?", userData, callback);

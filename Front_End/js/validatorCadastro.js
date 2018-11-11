75de307a1c97e2e946945bb56243a9f01a43397f
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-11-11 16:49:37 -0200

    Mobile number treatment

--- Front_End/js/validatorCadastro.js
+++ Front_End/js/validatorCadastro.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $('#dataNascimento').mask("99/99/9999"); //Tratando o Data
     var MobileBehavior = function (val) {
-            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+            return val.replace(/\D/g, '').length === 11 ? '(00)00000-0000' : '(00)0000-00009';
         },
         mobileOptions = {
             onKeyPress: function (val, e, field, options) {
